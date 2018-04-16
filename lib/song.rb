f7766a4a0b59c266585f81ec8ef97079a2eda1a1
Kaushal Patel <kaushalpatel778@gmail.com>
2018-04-16 02:19:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,11 @@ class Song
   end
   def self.find_by_name(name)
     self.all.find { |song| song.name == name}
-  
+
   end
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
-    
+
   end
   def self.all
     @@all
